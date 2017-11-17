3908e7c7be42799984cee12d279805919aedfe61
Pieter <p.kronemeijer@xs4all.nl>
2017-11-17 12:37:41 +0100

    fixed response listener

--- restaurant/app/src/main/java/com/example/pieter/restaurant/MainActivity.java
+++ restaurant/app/src/main/java/com/example/pieter/restaurant/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.pieter.restaurant;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-//import com.android.volley.*;
 import com.android.volley.toolbox.Volley;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,8 +16,6 @@ import com.android.volley.toolbox.StringRequest;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,23 +29,46 @@ public class MainActivity extends AppCompatActivity {
 
         // Request a string response from the provided URL.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        String[] courses = parseJson(response);
-                        setAdapter(courses);
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                setAdapter(new String[]{"Error!"});
-            }
-        });
+                new responsething(),
+                new responseerror()
+        );
+
         queue.add(stringRequest);
-
-
-
     }
+
+
+    private class responsething implements Response.Listener<String> {
+        @Override
+        public void onResponse(String response) {
+            String[] courses = parseJson(response);
+            setAdapter(courses);
+        }
+    }
+
+    private class responseerror implements Response.ErrorListener {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            setAdapter(new String[]{"Error!"});
+        }
+    }
+
+//        // Request a string response from the provided URL.
+//        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//                        String[] courses = parseJson(response);
+//                        setAdapter(courses);
+//                    }
+//                },
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        setAdapter(new String[]{"Error!"});
+//                }
+//        });
+
+
 
     private String[] parseJson(String response) {
         String[] result;
@@ -73,30 +91,29 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, courses);
         coursesview.setAdapter(adapter);
 
-        coursesview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MainActivity.this, foodlist.class);
-                intent.putExtra("course", adapterView.getItemAtPosition(i).toString());
-                startActivity(intent);
-            }
-        });
+        coursesview.setOnItemClickListener(new listfunctionthing());
+    }
+
+    private class listfunctionthing implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            Intent intent = new Intent(MainActivity.this, foodlist.class);
+            intent.putExtra("course", adapterView.getItemAtPosition(i).toString());
+            startActivity(intent);
+        }
+
+//        coursesview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(MainActivity.this, foodlist.class);
+//                intent.putExtra("course", adapterView.getItemAtPosition(i).toString());
+//                startActivity(intent);
+//            }
+//        });
     }
 
     public void toOrderPage(View view) {
         Intent intent = new Intent(this, check_order.class);
         startActivity(intent);
-
-//        System.out.println("YOU PUSHED THE BUTTON");
-//        SharedPreferences prefs = getSharedPreferences("settings", this.MODE_PRIVATE);
-//        int n = prefs.getInt("n", -1);
-//        System.out.println("n = " + n);
-//        System.out.println(prefs.getString("dish1", "ERROR"));
-//        System.out.println("GOING TO FOR LOOP");
-//        if (n != -1) {
-//            for (int i = 0; i < n; i++) {System.out.println("dish" + Integer.toString(i));
-//                System.out.println(prefs.getString("dish" + Integer.toString(i+1), null));
-//            }
-//        }
     }
 }
